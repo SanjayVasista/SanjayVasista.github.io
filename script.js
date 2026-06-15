d93e7fae90ced7020c14e22e13038ac1e788e00f
Sanjayvasista <148851443+SanjayVasista@users.noreply.github.com>
2026-06-15 22:41:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 
-console.log("Portfolio Website Loaded Successfully");
+window.addEventListener('scroll', () => {
+  const nav = document.querySelector('nav');
+
+  if(window.scrollY > 50){
+    nav.style.background = "rgba(5,8,22,0.7)";
+  } else {
+    nav.style.background = "transparent";
+  }
+});
